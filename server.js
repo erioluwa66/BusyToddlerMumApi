c95d0f63f1cede80e578c47f6a66d5ca2c2763ce
erioluwa66 <erioluwa66@gmail.com>
2024-03-21 13:57:00 -0400

    routes and working files created

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
+
+app.use(express.json());
+app.use(cors());
+
+const PORT = process.env.PORT || 5050;
+
+
+app.get("/", (req, res) => {
+  res.send("Hello from Express!");
+});
+
+app.listen(PORT, () => {
+  console.log(`running at http://localhost:${PORT}`);
+});
